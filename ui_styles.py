9ccd2dd1872fbf86cb988bfd4f322173ca6f816f
Sebastian Günther <sebgue@gmx.net>
2021-02-06 22:02:34 +0100

    [WP] reduce colors

--- ui_styles.py
+++ ui_styles.py
@@ -13,47 +13,45 @@
 ## https://doc.qt.io/qtforpython/licenses.html
 ##
 ################################################################################
+GRAY_01 = (40, 40, 40)
+WHITE_01 = (210, 210, 210)
+
 COLOR_01 = (27, 29, 35)
-COLOR_01a = (27, 29, 35, 160)
+COLOR_02 = (33, 37, 43)
+COLOR_03 = (35, 40, 49)
+COLOR_04 = (39, 44, 54)
+COLOR_05 = (43, 50, 61)
+COLOR_06 = (52, 59, 72)
+COLOR_07 = (58, 66, 81)
 
-COLOR_28 = (32, 34, 42)
-COLOR_03 = (33, 37, 43)
-COLOR_32 = (35, 40, 49)
+COLOR_08 = (65, 130, 195) # slider handle
 
-COLOR_12 = (39, 44, 54)
-COLOR_12a = (39, 44, 54, 150)
+COLOR_09 = (81, 255, 0) # light green, table header
 
-COLOR_06 = (40, 40, 40)
-COLOR_26 = (41, 45, 56)
-COLOR_33 = (43, 50, 61)
-COLOR_04 = (44, 49, 60)
+COLOR_11 = (85, 170, 255)
 
+COLOR_12 = (91, 101, 124)
+COLOR_13 = (94, 106, 130)
+COLOR_14 = (98, 103, 111)
 
-COLOR_08 = (52, 59, 72)
-COLOR_25 = (52, 58, 71)
-COLOR_09 = (55, 63, 77)
-COLOR_13 = (55, 62, 76)
-COLOR_30 = (57, 65, 80)
-COLOR_10 = (58, 66, 81)
-COLOR_31 = (61, 70, 86)
-
-COLOR_02 = (64, 71, 88)
-
-COLOR_15 = (65, 130, 195)
-
-COLOR_29 = (81, 255, 0)
-
-COLOR_05 = (85, 170, 255)
-
-COLOR_07 = (91, 101, 124)
-COLOR_11 = (94, 106, 130)
-COLOR_27 = (98, 103, 111)
-
-COLOR_14 = (105, 180, 255)
-
-COLOR_99 = (210, 210, 210)
+COLOR_15 = (105, 180, 255)
 
 
+
+# rgba 
+COLOR_01a = COLOR_01 +  (160,)
+COLOR_04a = COLOR_04 + (150,)
+
+# similar colors
+COLOR_102 = COLOR_02 # (32, 34, 42)
+COLOR_104 = COLOR_04 # (41, 45, 56) # Frame Div Content
+COLOR_105 = COLOR_05 # (44, 49, 60) 
+COLOR_106 = COLOR_06 # (52, 58, 71)
+COLOR_206 = COLOR_06 # (55, 62, 76)
+COLOR_306 = COLOR_06 # (55, 63, 77) # Scrollbar lines
+COLOR_107 = COLOR_07 # (57, 65, 80) # Push hover
+COLOR_207 = COLOR_07 # (61, 70, 86)
+COLOR_307 = COLOR_07 # (64, 71, 88) Combo hover
 
 class Style:
 
@@ -74,18 +72,18 @@ class Style:
         background-repeat: no-repeat;
         border: none;
         border-left: 28px solid rgb{COLOR_01};
-        border-right: 5px solid rgb{COLOR_04};
+        border-right: 5px solid rgb{COLOR_105};
         background-color: rgb{COLOR_01};
         text-align: left;
         padding-left: 45px;
     }}
     QPushButton:hover {{
-        background-color: rgb{COLOR_03};
-        border-left: 28px solid rgb{COLOR_03};
+        background-color: rgb{COLOR_02};
+        border-left: 28px solid rgb{COLOR_02};
     }}
     QPushButton:pressed {{
-        background-color: rgb{COLOR_05};
-        border-left: 28px solid rgb{COLOR_05};
+        background-color: rgb{COLOR_11};
+        border-left: 28px solid rgb{COLOR_11};
     }}
     """
 
@@ -94,7 +92,7 @@ class Style:
         QToolTip {{
         color: #ffffff;
         background-color: rgba{COLOR_01a};
-        border: 1px solid rgb{COLOR_06};
+        border: 1px solid rgb{GRAY_01};
         border-radius: 2px;
     }}
     """
@@ -108,28 +106,28 @@ class Style:
         padding-left: 10px;
     }}
     QLineEdit:hover {{
-        border: 2px solid rgb{COLOR_02};
+        border: 2px solid rgb{COLOR_307};
     }}
     QLineEdit:focus {{
-        border: 2px solid rgb{COLOR_07};
+        border: 2px solid rgb{COLOR_12};
     }}
     
     /* ######### SCROLL BARS ######### */
     QScrollBar:horizontal {{
         border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         height: 14px;
         margin: 0px 21px 0 21px;
         border-radius: 0px;
     }}
     QScrollBar::handle:horizontal {{
-        background: rgb{COLOR_05};
+        background: rgb{COLOR_11};
         min-width: 25px;
         border-radius: 7px
     }}
     QScrollBar::add-line:horizontal {{
         border: none;
-        background: rgb{COLOR_09};
+        background: rgb{COLOR_306};
         width: 20px;
         border-top-right-radius: 7px;
         border-bottom-right-radius: 7px;
@@ -138,7 +136,7 @@ class Style:
     }}
     QScrollBar::sub-line:horizontal {{
         border: none;
-        background: rgb{COLOR_09};
+        background: rgb{COLOR_306};
         width: 20px;
     
         border-top-left-radius: 7px;
@@ -154,19 +152,19 @@ class Style:
     }}
     QScrollBar:vertical {{
         border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         width: 14px;
         margin: 21px 0 21px 0;
         border-radius: 0px;
     }}
     QScrollBar::handle:vertical {{	
-        background: rgb{COLOR_05};
+        background: rgb{COLOR_11};
         min-height: 25px;
         border-radius: 7px
     }}
     QScrollBar::add-line:vertical {{
         border: none;
-        background: rgb{COLOR_09};
+        background: rgb{COLOR_306};
         height: 20px;
         border-bottom-left-radius: 7px;
         border-bottom-right-radius: 7px;
@@ -175,7 +173,7 @@ class Style:
     }}
     QScrollBar::sub-line:vertical {{
         border: none;
-        background: rgb{COLOR_09};
+        background: rgb{COLOR_306};
         height: 20px;
         border-top-left-radius: 7px;
         border-top-right-radius: 7px;
@@ -191,35 +189,35 @@ class Style:
     
     /* ######### CHECKBOX ######### */
     QCheckBox::indicator {{
-        border: 3px solid rgb{COLOR_08};
+        border: 3px solid rgb{COLOR_06};
         width: 15px;
         height: 15px;
         border-radius: 10px;
-        background: rgb{COLOR_04};
+        background: rgb{COLOR_105};
     }}
     QCheckBox::indicator:hover {{
-        border: 3px solid rgb{COLOR_10};
+        border: 3px solid rgb{COLOR_07};
     }}
     QCheckBox::indicator:checked {{
-        background: 3px solid rgb{COLOR_08};
-        border: 3px solid rgb{COLOR_08};	
+        background: 3px solid rgb{COLOR_06};
+        border: 3px solid rgb{COLOR_06};	
         background-image: url(:/16x16/icons/16x16/cil-check-alt.png);
     }}
     
     /* ######### RADIO BUTTON ######### */
     QRadioButton::indicator {{
-        border: 3px solid rgb{COLOR_08};
+        border: 3px solid rgb{COLOR_06};
         width: 15px;
         height: 15px;
         border-radius: 10px;
-        background: rgb{COLOR_04};
+        background: rgb{COLOR_105};
     }}
     QRadioButton::indicator:hover {{
-        border: 3px solid rgb{COLOR_10};
+        border: 3px solid rgb{COLOR_07};
     }}
     QRadioButton::indicator:checked {{
-        background: 3px solid rgb{COLOR_11};
-        border: 3px solid rgb{COLOR_08};	
+        background: 3px solid rgb{COLOR_13};
+        border: 3px solid rgb{COLOR_06};	
     }}
     
     /* ######### COMBOBOX ######### */
@@ -231,14 +229,14 @@ class Style:
         padding-left: 10px;
     }}
     QComboBox:hover{{
-        border: 2px solid rgb{COLOR_02};
+        border: 2px solid rgb{COLOR_307};
     }}
     QComboBox::drop-down {{
         subcontrol-origin: padding;
         subcontrol-position: top right;
         width: 25px; 
         border-left-width: 3px;
-        border-left-color: rgba{COLOR_12a};
+        border-left-color: rgba{COLOR_04a};
         border-left-style: solid;
         border-top-right-radius: 3px;
         border-bottom-right-radius: 3px;	
@@ -247,10 +245,10 @@ class Style:
         background-repeat: no-reperat;
     }}
     QComboBox QAbstractItemView {{
-        color: rgb{COLOR_05};	
+        color: rgb{COLOR_11};	
         background-color: rgb{COLOR_01};
         padding: 10px;
-        selection-background-color: rgb{COLOR_12};
+        selection-background-color: rgb{COLOR_04};
     }}
     
     /* ######### SLIDERS ######### */
@@ -258,13 +256,13 @@ class Style:
         border-radius: 9px;
         height: 18px;
         margin: 0px;
-        background-color: rgb{COLOR_08};
+        background-color: rgb{COLOR_06};
     }}
     QSlider::groove:horizontal:hover {{
-        background-color: rgb{COLOR_13};
+        background-color: rgb{COLOR_206};
     }}
     QSlider::handle:horizontal {{
-        background-color: rgb{COLOR_05};
+        background-color: rgb{COLOR_11};
         border: none;
         height: 18px;
         width: 18px;
@@ -272,23 +270,23 @@ class Style:
         border-radius: 9px;
     }}
     QSlider::handle:horizontal:hover {{
-        background-color: rgb{COLOR_14};
+        background-color: rgb{COLOR_15};
     }}
     QSlider::handle:horizontal:pressed {{
-        background-color: rgb{COLOR_15};
+        background-color: rgb{COLOR_08};
     }}
     
     QSlider::groove:vertical {{
         border-radius: 9px;
         width: 18px;
         margin: 0px;
-        background-color: rgb{COLOR_08};
+        background-color: rgb{COLOR_06};
     }}
     QSlider::groove:vertical:hover {{
-        background-color: rgb{COLOR_13};
+        background-color: rgb{COLOR_206};
     }}
     QSlider::handle:vertical {{
-        background-color: rgb{COLOR_05};
+        background-color: rgb{COLOR_11};
         border: none;
         height: 18px;
         width: 18px;
@@ -296,10 +294,10 @@ class Style:
         border-radius: 9px;
     }}
     QSlider::handle:vertical:hover {{
-        background-color: rgb{COLOR_14};
+        background-color: rgb{COLOR_15};
     }}
     QSlider::handle:vertical:pressed {{
-        background-color: rgb{COLOR_15};
+        background-color: rgb{COLOR_08};
     }}
     
     """
@@ -313,10 +311,10 @@ class Style:
         background-color: rgb{COLOR_01};
     }}
     QPushButton:hover {{
-        background-color: rgb{COLOR_03};
+        background-color: rgb{COLOR_02};
     }}
     QPushButton:pressed {{
-        background-color: rgb{COLOR_05};
+        background-color: rgb{COLOR_11};
     }}
     """
 
@@ -328,10 +326,10 @@ class Style:
     	background-color: transparent;
     }}
     QPushButton:hover {{
-    	background-color: rgb{COLOR_08};
+    	background-color: rgb{COLOR_06};
     }}
     QPushButton:pressed {{
-    	background-color: rgb{COLOR_05};
+    	background-color: rgb{COLOR_11};
     }}
     """
 
@@ -341,41 +339,41 @@ class Style:
     	background-color: transparent;
     }}
     QPushButton:hover {{
-    	background-color: rgb{COLOR_08};
+    	background-color: rgb{COLOR_06};
     }}
     QPushButton:pressed {{
-    	background-color: rgb{COLOR_05};
+    	background-color: rgb{COLOR_11};
     }}
     """
 
     # QFrame
-    style_frame_top_info = f"background-color: rgb{COLOR_12};"
+    style_frame_top_info = f"background-color: rgb{COLOR_04};"
 
     # QLabel
-    style_label_top_info = f"color: rgb{COLOR_27}; "
+    style_label_top_info = f"color: rgb{COLOR_14}; "
 
     ###################################################
 
     style_label_user_icon = f"""
     QLabel {{
         border-radius: 30px;
-        background-color: rgb{COLOR_04};
-        border: 5px solid rgb{COLOR_12};
+        background-color: rgb{COLOR_105};
+        border: 5px solid rgb{COLOR_04};
         background-position: center;
         background-repeat: no-repeat;
     }}
     """
 
-    style_frame_content_right = f"background-color: rgb{COLOR_04};"
+    style_frame_content_right = f"background-color: rgb{COLOR_105};"
 
     # QFrame
     style_frame = "border-radius: 5px;"
     style_frame_div_content_1 = f"""
-        background-color: rgb{COLOR_26}; 
+        background-color: rgb{COLOR_104}; 
         border-radius: 5px;
     """
 
-    style_frame_title_wid_1 = f"background-color: rgb{COLOR_12};"
+    style_frame_title_wid_1 = f"background-color: rgb{COLOR_04};"
 
     # QLineEdit
     style_lineEdit = f"""
@@ -386,37 +384,37 @@ class Style:
     	padding-left: 10px;
     }}
     QLineEdit:hover {{
-    	border: 2px solid rgb{COLOR_02};
+    	border: 2px solid rgb{COLOR_307};
     }}
     QLineEdit:focus {{
-    	border: 2px solid rgb{COLOR_07};
+    	border: 2px solid rgb{COLOR_12};
     }}
     """
 
     style_pushButton = f"""
     QPushButton {{
-    	border: 2px solid rgb{COLOR_08};
+    	border: 2px solid rgb{COLOR_06};
     	border-radius: 5px;	
-    	background-color: rgb{COLOR_08};
+    	background-color: rgb{COLOR_06};
     }}
     QPushButton:hover {{
-    	background-color: rgb{COLOR_30};
-    	border: 2px solid rgb{COLOR_31};
+    	background-color: rgb{COLOR_107};
+    	border: 2px solid rgb{COLOR_207};
     }}
     QPushButton:pressed {{
-    	background-color: rgb{COLOR_32};
-    	border: 2px solid rgb{COLOR_33};
+    	background-color: rgb{COLOR_03};
+    	border: 2px solid rgb{COLOR_05};
     }}
     """
 
-    style_labelVersion_3 = f"color: rgb{COLOR_27};"
+    style_labelVersion_3 = f"color: rgb{COLOR_14};"
 
-    style_frame_2 = f"background-color: rgb{COLOR_12}; border-radius: 5px;"
+    style_frame_2 = f"background-color: rgb{COLOR_04}; border-radius: 5px;"
 
     style_verticalScrollBar = f"""
     QScrollBar:vertical {{
     	border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         width: 14px;
         margin: 21px 0 21px 0;
     	border-radius: 0px;
@@ -430,14 +428,14 @@ class Style:
     }}
     QScrollBar:horizontal {{
         border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         height: 14px;
         margin: 0px 21px 0 21px;
     	border-radius: 0px;
     }}
     QScrollBar:vertical {{
     	border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         width: 14px;
         margin: 21px 0 21px 0;
     	border-radius: 0px;
@@ -451,10 +449,10 @@ class Style:
     	padding: 10px;
     }}
     QPlainTextEdit:hover {{
-    	border: 2px solid rgb{COLOR_02};
+    	border: 2px solid rgb{COLOR_307};
     }}
     QPlainTextEdit:focus {{
-    	border: 2px solid rgb{COLOR_07};
+    	border: 2px solid rgb{COLOR_12};
     }}
     """
 
@@ -467,20 +465,20 @@ class Style:
     	padding-left: 10px;
     }}
     QComboBox:hover{{
-    	border: 2px solid rgb{COLOR_02};
+    	border: 2px solid rgb{COLOR_307};
     }}
     QComboBox QAbstractItemView {{
-    	color: rgb{COLOR_05};	
+    	color: rgb{COLOR_11};	
     	background-color: rgb{COLOR_01};
     	padding: 10px;
-    	selection-background-color: rgb{COLOR_12};
+    	selection-background-color: rgb{COLOR_04};
     }}
     """
 
     style_horizontalScrollBar = f"""
     QScrollBar:horizontal {{
         border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         height: 14px;
         margin: 0px 21px 0 21px;
     	border-radius: 0px;
@@ -489,66 +487,66 @@ class Style:
 
     style_commandLinkButton = f"""
     QCommandLinkButton {{
-    	color: rgb{COLOR_05};
+    	color: rgb{COLOR_11};
     	border-radius: 5px;
     	padding: 5px;
     }}
     QCommandLinkButton:hover {{
-    	color: rgb{COLOR_99};
-    	background-color: rgb{COLOR_04};
+    	color: rgb{WHITE_01};
+    	background-color: rgb{COLOR_105};
     }}
     QCommandLinkButton:pressed {{
-    	color: rgb{COLOR_99};
-    	background-color: rgb{COLOR_25};
+    	color: rgb{WHITE_01};
+    	background-color: rgb{COLOR_106};
     }}
     """
 
     style_tableWidget = f"""
     QTableWidget {{	
-    	background-color: rgb{COLOR_12};
+    	background-color: rgb{COLOR_04};
     	padding: 10px;
     	border-radius: 5px;
-    	gridline-color: rgb{COLOR_04};
-    	border-bottom: 1px solid rgb{COLOR_04};
+    	gridline-color: rgb{COLOR_105};
+    	border-bottom: 1px solid rgb{COLOR_105};
     }}
     QTableWidget::item{{
-    	border-color: rgb{COLOR_04};
+    	border-color: rgb{COLOR_105};
     	padding-left: 5px;
     	padding-right: 5px;
-    	gridline-color: rgb{COLOR_04};
+    	gridline-color: rgb{COLOR_105};
     }}
     QTableWidget::item:selected{{
-    	background-color: rgb{COLOR_05};
+    	background-color: rgb{COLOR_11};
     }}
     QScrollBar:horizontal {{
         border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         height: 14px;
         margin: 0px 21px 0 21px;
     	border-radius: 0px;
     }}
     QScrollBar:vertical {{
     	border: none;
-        background: rgb{COLOR_08};
+        background: rgb{COLOR_06};
         width: 14px;
         margin: 21px 0 21px 0;
     	border-radius: 0px;
     }}
     QHeaderView::section{{
-    	Background-color: rgb{COLOR_12};
+    	Background-color: rgb{COLOR_04};
     	max-width: 30px;
-    	border: 1px solid rgb{COLOR_04};
+    	border: 1px solid rgb{COLOR_105};
     	border-style: none;
-        border-bottom: 1px solid rgb{COLOR_04};
-        border-right: 1px solid rgb{COLOR_04};
+        border-bottom: 1px solid rgb{COLOR_105};
+        border-right: 1px solid rgb{COLOR_105};
     }}
     
     QTableWidget::horizontalHeader {{
-    	background-color: rgb{COLOR_29};
+    	background-color: rgb{COLOR_09};
     }}
     QHeaderView::section:horizontal
     {{
-        border: 1px solid rgb{COLOR_28};
+        border: 1px solid rgb{COLOR_102};
     	background-color: rgb{COLOR_01};
     	padding: 3px;
     	border-top-left-radius: 7px;
@@ -556,14 +554,14 @@ class Style:
     }}
     QHeaderView::section:vertical
     {{
-        border: 1px solid rgb{COLOR_04};
+        border: 1px solid rgb{COLOR_105};
     }}
     """
 
-    style_frame_grip = f"background-color: rgb{COLOR_03};"
-    style_label_credits = f"color: rgb{COLOR_27};"
+    style_frame_grip = f"background-color: rgb{COLOR_02};"
+    style_label_credits = f"color: rgb{COLOR_14};"
 
-    style_label_version = f"color: rgb{COLOR_27};"
+    style_label_version = f"color: rgb{COLOR_14};"
     style_frame_size_grip = f"""
     QSizeGrip {{
     	background-image: url(:/16x16/icons/16x16/cil-size-grip.png);
